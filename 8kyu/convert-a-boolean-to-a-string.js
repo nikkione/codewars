1387897d4e56343dab159619a67f8bf01d9dde1c
Nikita Andreevich <xniki4x@gmail.com>
2023-11-11 09:25:32 +0300

    solution of task

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,13 @@
+// Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//SOLUTION
+
+function booleanToString(b) {
+  return b ? "true" : "false";
+}
+
+console.log("solution :", booleanToString(true));
